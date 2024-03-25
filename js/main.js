83de028b42675e6bbb2f44fe671c8c214212636b
0ceanSlim <chriskerr@gmx.com>
2024-03-25 10:11:23 -0400

    save theme preference to local storage

--- js/main.js
+++ js/main.js
@@ -8,12 +8,41 @@ import { fetchLongNotes, fetchUserProfile } from "./fetch";
 import { displayLongNote } from "./article";
 import { displayLongNoteIndex } from "./article_index";
 
-const darkModeToggle = document.getElementById("darkModeToggle");
-const html = document.documentElement;
+// Function to set dark mode preference
+function setDarkModePreference(isDarkMode) {
+  localStorage.setItem("darkMode", isDarkMode);
+}
 
+// Function to get dark mode preference
+function getDarkModePreference() {
+  return JSON.parse(localStorage.getItem("darkMode"));
+}
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+  const isDarkMode = getDarkModePreference();
+  const html = document.documentElement;
+  const darkModeToggle = document.getElementById("darkModeToggle");
+  if (isDarkMode) {
+    html.classList.add("dark");
+    darkModeToggle.querySelector("span").classList.add("translate-x-full");
+  } else {
+    html.classList.remove("dark");
+    darkModeToggle.querySelector("span").classList.remove("translate-x-full");
+  }
+}
+
+// On page load, apply dark mode preference
+window.addEventListener("load", () => {
+  toggleDarkMode();
+});
+
+// Event listener for dark mode toggle button
+const darkModeToggle = document.getElementById("darkModeToggle");
 darkModeToggle.addEventListener("click", () => {
-  html.classList.toggle("dark");
-  darkModeToggle.querySelector("span").classList.toggle("translate-x-full");
+  const isDarkMode = getDarkModePreference();
+  setDarkModePreference(!isDarkMode);
+  toggleDarkMode();
 });
 
 async function bootstrap() {
