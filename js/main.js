d7427e8f65228b7416c48d4ab42bd8d91865b717
Chris kerr <chriskerr@gmx.com>
2024-03-25 22:27:29 -0400

    comment out old darkmode method

--- js/main.js
+++ js/main.js
@@ -8,42 +8,42 @@ import { fetchLongNotes, fetchUserProfile } from "./fetch";
 import { displayLongNote } from "./article";
 import { displayLongNoteIndex } from "./article_index";
 
-// Function to set dark mode preference
-function setDarkModePreference(isDarkMode) {
-  localStorage.setItem("darkMode", isDarkMode);
-}
-
-// Function to get dark mode preference
-function getDarkModePreference() {
-  return JSON.parse(localStorage.getItem("darkMode"));
-}
-
-// Function to toggle dark mode
-function toggleDarkMode() {
-  const isDarkMode = getDarkModePreference();
-  const html = document.documentElement;
-  const darkModeToggle = document.getElementById("darkModeToggle");
-  if (isDarkMode) {
-    html.classList.add("dark");
-    darkModeToggle.querySelector("span").classList.add("translate-x-full");
-  } else {
-    html.classList.remove("dark");
-    darkModeToggle.querySelector("span").classList.remove("translate-x-full");
-  }
-}
-
-// On page load, apply dark mode preference
-window.addEventListener("load", () => {
-  toggleDarkMode();
-});
-
-// Event listener for dark mode toggle button
-const darkModeToggle = document.getElementById("darkModeToggle");
-darkModeToggle.addEventListener("click", () => {
-  const isDarkMode = getDarkModePreference();
-  setDarkModePreference(!isDarkMode);
-  toggleDarkMode();
-});
+//// Function to set dark mode preference
+//function setDarkModePreference(isDarkMode) {
+//  localStorage.setItem("darkMode", isDarkMode);
+//}
+//
+//// Function to get dark mode preference
+//function getDarkModePreference() {
+//  return JSON.parse(localStorage.getItem("darkMode"));
+//}
+//
+//// Function to toggle dark mode
+//function toggleDarkMode() {
+//  const isDarkMode = getDarkModePreference();
+//  const html = document.documentElement;
+//  const darkModeToggle = document.getElementById("darkModeToggle");
+//  if (isDarkMode) {
+//    html.classList.add("dark");
+//    darkModeToggle.querySelector("span").classList.add("translate-x-full");
+//  } else {
+//    html.classList.remove("dark");
+//    darkModeToggle.querySelector("span").classList.remove("translate-x-full");
+//  }
+//}
+//
+//// On page load, apply dark mode preference
+//window.addEventListener("load", () => {
+//  toggleDarkMode();
+//});
+//
+//// Event listener for dark mode toggle button
+//const darkModeToggle = document.getElementById("darkModeToggle");
+//darkModeToggle.addEventListener("click", () => {
+//  const isDarkMode = getDarkModePreference();
+//  setDarkModePreference(!isDarkMode);
+//  toggleDarkMode();
+//});
 
 async function bootstrap() {
   try {
